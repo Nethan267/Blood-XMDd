c304830ca31abee60ea6c9e9a9a02841ee6085d1
BLOOD-MAIN <Bloodxmd6@gmail.com>
2025-08-03 01:45:16 +0530

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -3,7 +3,7 @@ const { sleep } = require('../lib/functions');
 
 cmd({
     pattern: "restart",
-    desc: "Restart the bot KHAN-AI",
+    desc: "Restart the blood xmd",
     category: "owner",
     filename: __filename
 },
