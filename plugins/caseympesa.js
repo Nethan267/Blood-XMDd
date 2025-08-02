442c663c9461bc777788225cfdfcd5992790e26a
BLOOD-MAIN <Bloodxmd6@gmail.com>
2025-08-03 00:08:03 +0530

    Update caseympesa.js

--- plugins/caseympesa.js
+++ plugins/caseympesa.js
@@ -27,8 +27,8 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363302677217436@newsletter',
-                        newsletterName: "𝐂𝐀𝐒𝐄𝐘𝐑𝐇𝐎𝐃𝐄𝐒-𝐗𝐌𝐃 𝐏𝐀𝐘𝐌𝐄𝐍𝐓",
+                        newsletterJid: '120363419102725912@newsletter',
+                        newsletterName: "𝐁𝐋𝐎𝐎𝐃-𝐗𝐌𝐃 𝐏𝐀𝐘𝐌𝐄𝐍𝐓",
                         serverMessageId: 143
                     }
                 }
