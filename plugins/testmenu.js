915a398eb127a69c544f8c1ed65b0782f75abd68
BLOOD-MAIN <Bloodxmd6@gmail.com>
2025-08-06 09:03:35 +0530

    Update testmenu.js

--- plugins/testmenu.js
+++ plugins/testmenu.js
@@ -4,7 +4,7 @@ const { runtime } = require('../lib/functions');
 const axios = require('axios');
 
 cmd({
-    pattern: "allmenu",
+    pattern: "menu",
     desc: "Show interactive menu system",
     category: "menu",
     react: "🧾",
