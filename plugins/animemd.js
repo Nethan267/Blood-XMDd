f8c5d000acbae55aa57287952808ea69385bc52b
BLOOD-MAIN <Bloodxmd6@gmail.com>
2025-08-09 11:51:22 +0530

    Update animemd.js

--- plugins/animemd.js
+++ plugins/animemd.js
@@ -159,7 +159,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.url }, caption: '*ANIME GIRL IMAGE* 🥳\n\n\n *> © 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 🩸`*' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.url }, caption: '*ANIME GIRL IMAGE* 🥳\n\n\n > © 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 🩸' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`*Error Fetching Anime Girl image*: ${e.message}`);
@@ -199,7 +199,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.url }, caption: 'ANIME GIRL IMAGE 👾\n\n\n > ©💎 ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊-𝕏𝕄𝔻 💎' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.url }, caption: 'ANIME GIRL IMAGE 👾\n\n\n > © 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 🩸' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`*Error Fetching Anime Girl image*: ${e.message}`);
@@ -219,7 +219,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.url }, caption: 'ANIME GIRL IMAGE 👾\n\n\n > © 💎 ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊-𝕏𝕄𝔻 💎' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.url }, caption: 'ANIME GIRL IMAGE 👾\n\n\n > © 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 🩸' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`*Error Fetching Anime Girl image*: ${e.message}`);
@@ -239,7 +239,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.url }, caption: 'ANIME GIRL IMAGE 👾\n\n\n > © 💎 ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊-𝕏𝕄𝔻 💎' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.url }, caption: 'ANIME GIRL IMAGE 👾\n\n\n > © 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 🩸' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`*Error Fetching Anime Girl image*: ${e.message}`);
@@ -259,7 +259,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.url }, caption: 'ANIME GIRL IMAGE 👾\n\n\n > © 💎 ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊-𝕏𝕄𝔻 💎' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.url }, caption: 'ANIME GIRL IMAGE 👾\n\n\n > © 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 🩸' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`*Error Fetching Anime Girl image*: ${e.message}`);
@@ -280,7 +280,7 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-let dec = `>💎 ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊-𝕏𝕄𝔻 💎 ANIME IMGS*`
+let dec = `> 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 𝘼𝙉𝙄𝙈𝙀 𝙄𝙈𝙂𝙎 🩸`
 await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/b26f27aa5daaada031b90.jpg`},caption:dec},{quoted:mek});
 await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/51b44e4b086667361061b.jpg`},caption:dec},{quoted:mek});
 await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/7d165d73f914985542537.jpg`},caption:dec},{quoted:mek});
@@ -306,15 +306,15 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/aD7t0Bc.jpg` },caption: '> © 💎 ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊-𝕏𝕄𝔻 💎' },{quoted:mek});
+await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/aD7t0Bc.jpg` },caption: '> © 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 🩸' },{quoted:mek});
 
-await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/PQO5wPN.jpg` },caption: '> © 💎 ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊-𝕏𝕄𝔻 💎' },{quoted:mek});
+await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/PQO5wPN.jpg` },caption: '> © 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 🩸' },{quoted:mek});
 
-await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/5At1P4A.jpg` },caption: '> © 💎 ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊-𝕏𝕄𝔻 💎' },{quoted:mek});
+await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/5At1P4A.jpg` },caption: '> © 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 🩸' },{quoted:mek});
 
-await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/MjtH3Ha.jpg` },caption: '> © 💎 ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊-𝕏𝕄𝔻 💎' },{quoted:mek});
+await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/MjtH3Ha.jpg` },caption: '> © 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 🩸' },{quoted:mek});
 
-await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/QQW7VKy.jpg` },caption: '> © 💎 ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊-𝕏𝕄𝔻 💎' },{quoted:mek});
+await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/QQW7VKy.jpg` },caption: '> © 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 🩸' },{quoted:mek});
 
 }catch(e){
 console.log(e)
@@ -332,15 +332,15 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/0r1Bn88.jpg` },caption: '> © 💎 ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊-𝕏𝕄𝔻 💎' },{quoted:mek});
+await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/0r1Bn88.jpg` },caption: '> © 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 🩸' },{quoted:mek});
 
-await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/2Xdpuov.png` },caption: '> © 💎 ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊-𝕏𝕄𝔻 💎' },{quoted:mek});
+await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/2Xdpuov.png` },caption: '> © 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 🩸' },{quoted:mek});
 
-await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/0hx-3AP.png` },caption: '> © 💎 ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊-𝕏𝕄𝔻 💎' },{quoted:mek});
+await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/0hx-3AP.png` },caption: '> © 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 🩸' },{quoted:mek});
 
-await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/q054x0_.png` },caption: '> © 💎 ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊-𝕏𝕄𝔻 💎' },{quoted:mek});
+await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/q054x0_.png` },caption: '> © 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 🩸' },{quoted:mek});
 
-await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/4lyqRvd.jpg` },caption: '> © 💎 ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊-𝕏𝕄𝔻 💎' },{quoted:mek});
+await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/4lyqRvd.jpg` },caption: '> © 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 🩸' },{quoted:mek});
 
 }catch(e){
 console.log(e)
@@ -359,15 +359,15 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/gnpc_Lr.jpeg` },caption: '> ©💎 ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊-𝕏𝕄𝔻 💎' },{quoted:mek});
+await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/gnpc_Lr.jpeg` },caption: '> © 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 🩸' },{quoted:mek});
 
-await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/P6X-ph6.jpg` },caption: '> © 💎 ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊-𝕏𝕄𝔻 💎' },{quoted:mek});
+await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/P6X-ph6.jpg` },caption: '> © 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 🩸' },{quoted:mek});
 
-await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/~p5W9~k.png` },caption: '> © 💎 ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊-𝕏𝕄𝔻 💎' },{quoted:mek});
+await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/~p5W9~k.png` },caption: '> © 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 🩸' },{quoted:mek});
 
-await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/7Apu5C9.jpg` },caption: '> © 💎 ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊-𝕏𝕄𝔻 💎' },{quoted:mek});
+await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/7Apu5C9.jpg` },caption: '> © 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 🩸' },{quoted:mek});
 
-await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/OTRfON6.jpg` },caption: '> © 💎 ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊-𝕏𝕄𝔻 💎' },{quoted:mek});
+await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/OTRfON6.jpg` },caption: '> © 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 🩸' },{quoted:mek});
 
 }catch(e){
 console.log(e)
@@ -386,15 +386,15 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/aGgUm80.jpg` },caption: '> © 💎 ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊-𝕏𝕄𝔻 💎' },{quoted:mek});
+await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/aGgUm80.jpg` },caption: '> © 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 🩸' },{quoted:mek});
 
-await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/i~RQhRD.png` },caption: '> © 💎 ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊-𝕏𝕄𝔻 💎' },{quoted:mek});
+await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/i~RQhRD.png` },caption: '> © 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 🩸' },{quoted:mek});
 
-await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/94LH-aU.jpg` },caption: '> © 💎 ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊-𝕏𝕄𝔻 💎' },{quoted:mek});
+await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/94LH-aU.jpg` },caption: '> © 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 🩸' },{quoted:mek});
 
-await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/V8hvqfK.jpg` },caption: '> © 💎 ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊-𝕏𝕄𝔻 💎' },{quoted:mek});
+await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/V8hvqfK.jpg` },caption: '> © 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 🩸' },{quoted:mek});
 
-await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/lMiXE7j.png` },caption: '> © 💎 ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊-𝕏𝕄𝔻 💎' },{quoted:mek});
+await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/lMiXE7j.png` },caption: '> © 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 🩸' },{quoted:mek});
 
 }catch(e){
 console.log(e)
@@ -413,15 +413,15 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/-ABlAvr.jpg` },caption: '> © 💎 ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊-𝕏𝕄𝔻 💎' },{quoted:mek});
+await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/-ABlAvr.jpg` },caption: '> © 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 🩸' },{quoted:mek});
 
-await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/HNEg0-Q.png` },caption: '> © 💎 ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊-𝕏𝕄𝔻 💎' },{quoted:mek});
+await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/HNEg0-Q.png` },caption: '> © 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 🩸' },{quoted:mek});
 
-await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/3x~ovC6.jpg` },caption: '> © 💎 ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊-𝕏𝕄𝔻 💎' },{quoted:mek});
+await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/3x~ovC6.jpg` },caption: '> © 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 🩸' },{quoted:mek});
 
-await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/brv-GJu.jpg` },caption: '> © 💎 ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊-𝕏𝕄𝔻 💎' },{quoted:mek});
+await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/brv-GJu.jpg` },caption: '> © 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 🩸' },{quoted:mek});
 
-await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/FWE8ggD.png` },caption: '> © 💎 ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊-𝕏𝕄𝔻 💎' },{quoted:mek});
+await conn.sendMessage(from,{image :{ url: `https://i.waifu.pics/FWE8ggD.png` },caption: '> © 𝙋𝙊𝙒𝙀𝙍𝘿 𝘽𝙔 𝘽𝙇𝙊𝙊𝘿 𝙓𝙈𝘿 🩸' },{quoted:mek});
 
 }catch(e){
 console.log(e)
