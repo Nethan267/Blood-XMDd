12acec9315e485c095a5512519648c277dad9e38
BLOOD-MAIN <Bloodxmd6@gmail.com>
2025-08-27 06:52:52 +0530

    Update env.js

--- plugins/env.js
+++ plugins/env.js
@@ -24,25 +24,136 @@ async (conn, mek, m, { from, quoted, reply }) => {
 ┃▸┃๏ *ENV SETTINGS 👻*
 ┃▸└───────────···๏
 ╰────────────────┈⊷
-╭━━〔 *Enabled Disabled* 〕━━┈⊷
-┇๏ *Auto Read Status:* ${isEnabled(config.AUTO_STATUS_SEEN) ? "Enabled ✅" : "Disabled ❌"}
-┇๏ *Auto Reply Status:* ${isEnabled(config.AUTO_STATUS_REPLY) ? "Enabled ✅" : "Disabled ❌"}
-┇๏ *Auto Reply:* ${isEnabled(config.AUTO_REPLY) ? "Enabled ✅" : "Disabled ❌"}
-┇๏ *Auto Sticker:* ${isEnabled(config.AUTO_STICKER) ? "Enabled ✅" : "Disabled ❌"}
-┇๏ *Auto Voice:* ${isEnabled(config.AUTO_VOICE) ? "Enabled ✅" : "Disabled ❌"}
-┇๏ *Owner React:* ${isEnabled(config.OWNER_REACT) ? "Enabled ✅" : "Disabled ❌"}
-┇๏ *Custom Reacts:* ${isEnabled(config.CUSTOM_REACT) ? "Enabled ✅" : "Disabled ❌"}
-┇๏ *Auto React:* ${isEnabled(config.AUTO_REACT) ? "Enabled ✅" : "Disabled ❌"}
-┇๏ *Delete Links:* ${isEnabled(config.DELETE_LINKS) ? "Enabled ✅" : "Disabled ❌"}
-┇๏ *Anti-Link:* ${isEnabled(config.ANTI_LINK) ? "Enabled ✅" : "Disabled ❌"}
-┇๏ *Anti-Bad Words:* ${isEnabled(config.ANTI_BAD) ? "Enabled ✅" : "Disabled ❌"}
-┇๏ *Auto Typing:* ${isEnabled(config.AUTO_TYPING) ? "Enabled ✅" : "Disabled ❌"}
-┇๏ *Auto Recording:* ${isEnabled(config.AUTO_RECORDING) ? "Enabled ✅" : "Disabled ❌"}
-┇๏ *Always Online:* ${isEnabled(config.ALWAYS_ONLINE) ? "Enabled ✅" : "Disabled ❌"}
-┇๏ *Public Mode:* ${isEnabled(config.PUBLIC_MODE) ? "Enabled ✅" : "Disabled ❌"}
-┇๏ *Read Message:* ${isEnabled(config.READ_MESSAGE) ? "Enabled ✅" : "Disabled ❌"}
-╰━━━━━━━━━━━━──┈⊷
-> ${config.DESCRIPTION}`;
+
+
+╭══════════════════════○
+┣━ *𝗪𝗢𝗥𝗞 𝗠𝗢𝗗𝗘 😈*
+> *1️⃣.1️⃣  Public Work*
+> *1️⃣.2️⃣  Private Work*
+> *1️⃣.3️⃣  Group Only*
+> *1️⃣.4️⃣  Inbox Only*
+╭══════════════════════○
+┣━ *𝗔𝗨𝗧𝗢 𝗩𝗢𝗜𝗖𝗘 😈*
+> *2️⃣.1️⃣ Auto Voice On*
+> *2️⃣.2️⃣ Auto Voice Off*
+╭══════════════════════○
+┣━ *𝗔𝗨𝗧𝗢 𝗦𝗧𝗔𝗧𝗨𝗦 𝗦𝗘𝗘𝗡 😈*
+> *3️⃣.1️⃣ Auto Read Status On*
+> *3️⃣.2️⃣ Auto Read Status Off*
+╭══════════════════════○
+┣━ *𝗔𝗨𝗧𝗢 𝗦𝗧𝗜𝗖𝗞𝗘𝗥 😈*
+> *4️⃣.1️⃣ Auto sticker On*
+> *4️⃣.2️⃣ Auto sticker Off*
+╭══════════════════════○
+┣━ *𝗔𝗨𝗧𝗢 𝗥𝗘𝗣𝗟𝗬😈*
+> *5️⃣.1️⃣ Auto reply On*
+> *5️⃣.2️⃣ Auto reply Off*
+╭══════════════════════○
+┣━ *𝗕𝗢𝗧 𝗢𝗡𝗟𝗜𝗡𝗘 𝗢𝗙𝗙𝗟𝗜𝗡𝗘 😈*
+> *6️⃣.1️⃣ Online On*
+> *6️⃣.2️⃣ Online Off*
+╭══════════════════════○
+┣━ *𝗠𝗦𝗚 𝗥𝗘𝗔𝗗 😈*
+> *7️⃣.1️⃣ Read Msg On*
+> *7️⃣.2️⃣ Read Msg Off*
+╭══════════════════════○
+┣━ *𝗠𝗦𝗚 𝗥𝗘𝗔𝗖𝗧 😈*
+> *8️⃣.1️⃣ Auto React On*
+> *8️⃣.2️⃣ Auto React Off*
+╭══════════════════════○
+┣━ *𝗔𝗡𝗧𝗜 𝗟𝗜𝗡𝗞 😈*
+> *9️⃣.1️⃣ Anti Link On*
+> *9️⃣.2️⃣ Anti Link Off*
+> *9️⃣.3️⃣ Anti Link Remove*
+╰═══════════════════════○
+
+
+* *🔢 Reply Below This Number Change To Bot Change Setting*
+
+> *🄿🄾🅆🄴🅁🄳 🅱🆈 SULA*`;
+
+        const vv = await conn.sendMessage(from, { image: { url: "YOUR PHOTO URL"}, caption: desc }, { quoted: mek });
+
+        conn.ev.on('messages.upsert', async (msgUpdate) => {
+            const msg = msgUpdate.messages[0];
+            if (!msg.message || !msg.message.extendedTextMessage) return;
+
+            const selectedOption = msg.message.extendedTextMessage.text.trim();
+
+            if (msg.message.extendedTextMessage.contextInfo && msg.message.extendedTextMessage.contextInfo.stanzaId === vv.key.id) {
+                switch (selectedOption) {
+                    case '1.1':
+                        reply(".update MODE:public" );
+                        break;
+                    case '1.2':               
+                        reply(".update MODE:private");
+                        break;
+                    case '1.3':               
+                          reply(".update MODE:group");
+                      break;
+                    case '1.4':     
+                        reply(".update MODE:inbox");
+                      break;
+                    case '2.1':     
+                        reply(".update AUTO_VOICE:true");
+                        break;
+                    case '2.2':     
+                        reply(".update AUTO_VOICE:false");
+                    break;
+                    case '3.1':    
+                        reply(".update AUTO_READ_STATUS:true");
+                    break;
+                    case '3.2':    
+                        reply(".update AUTO_READ_STATUS:false");
+                    break;                    
+                    case '4.1':    
+                        reply(".update AUTO_STICKER:true");
+                    break;
+                    case '4.2':    
+                        reply(".update AUTO_STICKER:false");
+                    break;                                        
+                    case '5.1':    
+                        reply(".update AUTO_REPLY:true");
+                    break;
+                    case '5.2':    
+                        reply(".update AUTO_REPLY:false");
+                    break;                        
+                    case '6.1':    
+                        reply(".update ALLWAYS_OFFLINE:true");
+                    break; 
+                    case '6.2':    
+                        reply(".update ALLWAYS_OFFLINE:false");
+                    break;                       
+                    case '7.1':    
+                        reply(".update READ_MESSAGE:true");
+                    break;
+                    case '7.2':    
+                        reply(".update READ_MESSAGE:false");
+                    break;
+                    case '8.1':    
+                        reply(".update config.AUTO_REACT:true");
+                    break;
+                    case '8.2':    
+                        reply(".update config.AUTO_REACT:false");
+                    break;
+                    case '9.1':    
+                        reply(".update ANTI_LINK:true");
+                        reply(".update ANTI_LINKK:false");
+                    break;
+                    case '9.2':    
+                        reply(".update ANTI_LINKK:true");
+                        reply(".update ANTI_LINK:false");
+                    break;
+                    case '9.3':    
+                        reply(".update ANTI_LINK:false");
+                        reply(".update ANTI_LINKK:false");
+                    break;
+                    default:
+                        reply("Invalid option. Please select a valid option🔴");
+                }
+
+            }
+        });
 
         // Send message with an image
         await conn.sendMessage(
