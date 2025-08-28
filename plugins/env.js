b13de05b4b0791ef1b03a3fc42c89ad2148a58cf
BLOOD-MAIN <Bloodxmd6@gmail.com>
2025-08-28 11:06:47 +0530

    Update env.js

--- plugins/env.js
+++ plugins/env.js
@@ -24,49 +24,25 @@ async (conn, mek, m, { from, quoted, reply }) => {
 ┃▸┃๏ *ENV SETTINGS 👻*
 ┃▸└───────────···๏
 ╰────────────────┈⊷
-
-
-╭══════════════════════○
-┣━ *𝗪𝗢𝗥𝗞 𝗠𝗢𝗗𝗘 😈*
-> *1️⃣.1️⃣  Public Work*
-> *1️⃣.2️⃣  Private Work*
-> *1️⃣.3️⃣  Group Only*
-> *1️⃣.4️⃣  Inbox Only*
-╭══════════════════════○
-┣━ *𝗔𝗨𝗧𝗢 𝗩𝗢𝗜𝗖𝗘 😈*
-> *2️⃣.1️⃣ Auto Voice On*
-> *2️⃣.2️⃣ Auto Voice Off*
-╭══════════════════════○
-┣━ *𝗔𝗨𝗧𝗢 𝗦𝗧𝗔𝗧𝗨𝗦 𝗦𝗘𝗘𝗡 😈*
-> *3️⃣.1️⃣ Auto Read Status On*
-> *3️⃣.2️⃣ Auto Read Status Off*
-╭══════════════════════○
-┣━ *𝗔𝗨𝗧𝗢 𝗦𝗧𝗜𝗖𝗞𝗘𝗥 😈*
-> *4️⃣.1️⃣ Auto sticker On*
-> *4️⃣.2️⃣ Auto sticker Off*
-╭══════════════════════○
-┣━ *𝗔𝗨𝗧𝗢 𝗥𝗘𝗣𝗟𝗬😈*
-> *5️⃣.1️⃣ Auto reply On*
-> *5️⃣.2️⃣ Auto reply Off*
-╭══════════════════════○
-┣━ *𝗕𝗢𝗧 𝗢𝗡𝗟𝗜𝗡𝗘 𝗢𝗙𝗙𝗟𝗜𝗡𝗘 😈*
-> *6️⃣.1️⃣ Online On*
-> *6️⃣.2️⃣ Online Off*
-╭══════════════════════○
-┣━ *𝗠𝗦𝗚 𝗥𝗘𝗔𝗗 😈*
-> *7️⃣.1️⃣ Read Msg On*
-> *7️⃣.2️⃣ Read Msg Off*
-╭══════════════════════○
-┣━ *𝗠𝗦𝗚 𝗥𝗘𝗔𝗖𝗧 😈*
-> *8️⃣.1️⃣ Auto React On*
-> *8️⃣.2️⃣ Auto React Off*
-╭══════════════════════○
-┣━ *𝗔𝗡𝗧𝗜 𝗟𝗜𝗡𝗞 😈*
-> *9️⃣.1️⃣ Anti Link On*
-> *9️⃣.2️⃣ Anti Link Off*
-> *9️⃣.3️⃣ Anti Link Remove*
-╰═══════════════════════○
-
+╭━━〔 *Enabled Disabled* 〕━━┈⊷
+┇๏ *Auto Read Status:* ${isEnabled(config.AUTO_STATUS_SEEN) ? "Enabled ✅" : "Disabled ❌"}
+┇๏ *Auto Reply Status:* ${isEnabled(config.AUTO_STATUS_REPLY) ? "Enabled ✅" : "Disabled ❌"}
+┇๏ *Auto Reply:* ${isEnabled(config.AUTO_REPLY) ? "Enabled ✅" : "Disabled ❌"}
+┇๏ *Auto Sticker:* ${isEnabled(config.AUTO_STICKER) ? "Enabled ✅" : "Disabled ❌"}
+┇๏ *Auto Voice:* ${isEnabled(config.AUTO_VOICE) ? "Enabled ✅" : "Disabled ❌"}
+┇๏ *Owner React:* ${isEnabled(config.OWNER_REACT) ? "Enabled ✅" : "Disabled ❌"}
+┇๏ *Custom Reacts:* ${isEnabled(config.CUSTOM_REACT) ? "Enabled ✅" : "Disabled ❌"}
+┇๏ *Auto React:* ${isEnabled(config.AUTO_REACT) ? "Enabled ✅" : "Disabled ❌"}
+┇๏ *Delete Links:* ${isEnabled(config.DELETE_LINKS) ? "Enabled ✅" : "Disabled ❌"}
+┇๏ *Anti-Link:* ${isEnabled(config.ANTI_LINK) ? "Enabled ✅" : "Disabled ❌"}
+┇๏ *Anti-Bad Words:* ${isEnabled(config.ANTI_BAD) ? "Enabled ✅" : "Disabled ❌"}
+┇๏ *Auto Typing:* ${isEnabled(config.AUTO_TYPING) ? "Enabled ✅" : "Disabled ❌"}
+┇๏ *Auto Recording:* ${isEnabled(config.AUTO_RECORDING) ? "Enabled ✅" : "Disabled ❌"}
+┇๏ *Always Online:* ${isEnabled(config.ALWAYS_ONLINE) ? "Enabled ✅" : "Disabled ❌"}
+┇๏ *Public Mode:* ${isEnabled(config.PUBLIC_MODE) ? "Enabled ✅" : "Disabled ❌"}
+┇๏ *Read Message:* ${isEnabled(config.READ_MESSAGE) ? "Enabled ✅" : "Disabled ❌"}
+╰━━━━━━━━━━━━──┈⊷
+> ${config.DESCRIPTION}`;
 
         // Send message with an image
         await conn.sendMessage(
